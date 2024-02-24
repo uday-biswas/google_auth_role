3b24c1b0a844b4ad896c30f81e3de37f866af9c7
uday biswas <ub8280634471@gmail.com>
2024-02-24 23:00:03 +0530

    added backend swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
@@ -19,6 +20,14 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4000);
 }
 bootstrap();
